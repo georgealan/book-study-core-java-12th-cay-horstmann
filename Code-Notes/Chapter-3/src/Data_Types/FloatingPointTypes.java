7b2563db9ffedb5abb273b6eaa9d0c247a9afcc6
George Alan <georgealanrufo@gmail.com>
2022-04-16 22:47:21 -0300

    Study Day Finished! 16/04/2022

---
+++ Code-Notes/Chapter-3/src/Data_Types/FloatingPointTypes.java
@@ -0,0 +1,41 @@
+package Data_Types;
+
+import java.util.Arrays;
+
+/**
+ * The floating-point numbers are numbers with fractional parts, they're 2 types: float and double.
+ *
+ * Numbers of type float have a suffix F or f example: 3.14F, Flaating numbers without a suffix such as 3.14 will be
+ * considered of type double. Optionaly you can supply the D or d suffix, example: 3.14D.
+ *
+ *  Floting-point numbers are nor suitable for financial calculations, in wich roundoff errors cannot be tolerated, if
+ *  need precise numerical computations use the BigDecimal class.
+ */
+
+public class FloatingPointTypes {
+    public static void main(String[] args) {
+
+        float ammountInCash; // 4 bytes, over 3.40282347x10^38 (6-7 decimal digits)
+        float ammountInCash2;
+        double ammountInCashRichGuy; // 8 bytes, over 1.79769313486231570x10^308 (15 decimal digits)
+        double ammountInCashRichGuy2;
+        double ammountInCashRichGuy3;
+        double ammountInCashRichGuy4;
+
+        ammountInCash = 1.234F;
+        ammountInCash += 1.234;
+        ammountInCash2 = 1234;
+        ammountInCashRichGuy = 1.234;
+        ammountInCashRichGuy2 = 1.234E2;
+        ammountInCashRichGuy3 = 1.234E3;
+        ammountInCashRichGuy4 = 1.234E4;
+
+        System.out.println(ammountInCash);
+        System.out.println(ammountInCash2);
+        System.out.println(ammountInCashRichGuy);
+        System.out.println(ammountInCashRichGuy2);
+        System.out.println(ammountInCashRichGuy3);
+        System.out.println(ammountInCashRichGuy4);
+
+    }
+}
---
+++ Code-Notes/Chapter-3/src/Data_Types/IntegerTypes.java
@@ -0,0 +1,20 @@
+package Data_Types;
+
+/** STUDY NOTES
+ * The integer parts are from numbers without fractional parts. Negative values are allowed, are 4 interger types
+ * exitent, they are: int, short, long and byte.
+ *
+ */
+
+public class IntegerTypes {
+    public static void main(String[] args) {
+        // Example variables with integer types
+        byte peopleInHome; // 1 byte, Range is: -128 to 127
+        short peopleInCity; // 2 bytes, Range is: -32,768 to 32,767
+        int peopleInState; // 4 bytes, Range is: -2,147,483,648 to 2,147,483,647 (just over 2 billion)
+        long peopleInCountry; // 8 bytes, Range is: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+
+
+    }
+}
